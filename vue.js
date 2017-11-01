340a125b3845fe21cd9e564d9f7e89db096b0a5e
Unknown <ro0504008@otc.edu>
2017-11-01 01:31:54 -0500

    done me thinks

--- vue.js
+++ vue.js
@@ -1,45 +1,94 @@
 $(function(){
-	let vm = new Vue({
-		el: '#app',
-		data:{
-			racing: true,
+   
+     let vm = new Vue({
+        el: '#raceCode',
+        data: {
+			racing: false,
 			winner: null,
+			pika: 0,
 			charmander: 0,
-			pikachu: 0,
-			tick: 0,
-			interval: null
-		},
-		computed: {
-			winning(){
-				if (this.charmander == this.pikachu) return null
-				
-				return this.charmander > this.pikachu ? 'charmander' 'pikachu'
-			}
-		}
-		
-		
-		
-		
-		
-	})
-	
-	var hidey = new Vue({
-		el: 'hidey',
-		
-		if (racing == false){
-			data: {
-				seen: true
-			}
-		}
-		else{
-			data: {
-				seen: false
-			}
-		}
-	})
-	
-	
-	
-	
-	
+			counter: 0,
+			interval: null,
+			pikawin: false,
+			charwin: false,
+			place: false,
+        },
+		 
+        methods: {
+            letsRace() {
+                this.racing = true
+                this.interval = setInterval(() => {
+                    this.moveRacer()
+                }, 100)
+            },
+
+            moveRacer() {
+                this.counter++
+                this.pika += (Math.random() >= Math.random()) ? 1 : 0
+                this.charmander += (Math.random() >= Math.random()) ? 1 : 0
+                this.whoWon()
+            },
+
+            whoWon() {
+                if (this.pika == this.charmander) return
+                if (this.pika >= 90) {
+                    this.Winning('Pikachu')
+					this.pikawin = true
+					this.place = true
+                }
+                if (this.charmander >= 90) {
+                    this.Winning('Charmander')
+					this.charwin = true
+					this.place = true
+                }
+            },
+
+            Winning(player) {
+                clearInterval(this.interval)
+                this.interval = null
+                /* this.racing = false */
+                this.winner = player
+            },
+
+            restart() {
+                this.racing = false
+                this.winner = null
+                this.pika = 0
+                this.charmander = 0
+                this.counter = 0
+				this.pikawin = false
+				this.charwin = false
+				this.place = false
+            }
+        },
+
+        computed: {
+            pikaRace() {
+                return {
+                    left: `${this.pika}vw`
+                }
+            },
+
+            pikaWinTest() {
+                if (!this.winner) return
+					return this.winner == 'pikachU'
+            },
+
+            charRace() {
+                return {
+                    left: `${this.charmander}vw`
+                }
+            },
+
+            charWinTest() {
+                if (!this.winner) return
+					return this.winner == 'charmandeR'
+            },
+
+            winner() {
+                if (this.pika == this.charmander) return null
+                    return this.pika > this.charmander ? 'Pikachu':'Charmander'
+            }
+        },
+     })
 })
